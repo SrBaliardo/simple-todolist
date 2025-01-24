c8062784a39413f83b30caeeed9bb95aa5381160
SrBaliardo <tao.john007@icloud.com>
2025-01-24 11:31:27 -0300

    feat(code): added README and body background.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Você é daqueles que precisam de lembretes para não esquecer de suas tarefas?
+
+Fácil! Utilize essa lista de tarefas simplificada.
+
+Basta adicionar uma tarefa e você pode marcar como conluída ou excluir após execução!
+
+Essa lista não vai desaparecer mesmo se você fechar a página ou desligar seu computador, não se esqueça nem daquelas que ficaram para trás.
+
+<br>
+
+# Are you one of those people who needs reminders so you don't forget your tasks?
+
+Easy! Use this simplified task list.
+
+Just add a task and you can mark it as completed or delete it once it's done!
+
+This list won't disappear even if you close the page or switch off your computer, so don't forget the ones you've left behind.
+
+You can view the project via the link: <br>
+https://srbaliardo.github.io/simple-todolist/
+
+<br>
+
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-const input = document.querySelector('.input-task');
-const buttonAdd = document.querySelector('.add-task');
-const listTasks = document.querySelector('.list-tasks');
+const input = document.querySelector(".input-task");
+const buttonAdd = document.querySelector(".add-task");
+const listTasks = document.querySelector(".list-tasks");
 
-let taskList = []
+let taskList = [];
 
 function addNewTask() {
-    taskList.push({
-        task: input.value,
-        check: false
-    })
+  taskList.push({
+    task: input.value,
+    check: false,
+  });
 
-    input.value = ''
+  input.value = "";
 
-    addedTasks()
-
+  addedTasks();
 }
 
 function addedTasks() {
-    let newTask = ''
+  let newTask = "";
 
-    taskList.forEach((task, index) => {
-        newTask = newTask + `
+  taskList.forEach((task, index) => {
+    newTask =
+      newTask +
+      `
             <li class="task ${task.check && "done"}">
                 <img src="./images/checked.png" alt="check-icon" onclick="checkTask(${index})">
                 <p>${task.task}</p>
                 <img src="./images/trash.png" alt="trash-icon" onclick="deleteTask(${index})">
             </li>
-        `
-    })
+        `;
+  });
 
-    listTasks.innerHTML = newTask
+  listTasks.innerHTML = newTask;
 
-    localStorage.setItem('list', JSON.stringify(taskList))
-    
+  localStorage.setItem("list", JSON.stringify(taskList));
 }
 
 function checkTask(index) {
-    taskList[index].check = !taskList[index].check
+  taskList[index].check = !taskList[index].check;
 
-    addedTasks()
+  addedTasks();
 }
 
 function deleteTask(index) {
-    taskList.splice(index, 1)
-    
-    addedTasks()
+  taskList.splice(index, 1);
+
+  addedTasks();
 }
 
 function reloadTasks() {
-    const storageTasks = localStorage.getItem('list')
+  const storageTasks = localStorage.getItem("list");
 
-    if (storageTasks) {
-        taskList = JSON.parse(storageTasks)
-    }
-     
-    addedTasks()
+  if (storageTasks) {
+    taskList = JSON.parse(storageTasks);
+  }
+
+  addedTasks();
 }
 
-reloadTasks()
-buttonAdd.addEventListener('click', addNewTask);
+reloadTasks();
+buttonAdd.addEventListener("click", addNewTask);
